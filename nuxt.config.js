e4dd217e946e9af5681533ba0c8b054a1ff7c9cf
Yuta Takahashi <penityai@gmail.com>
2020-01-07 01:57:38 +0900

    Added Font Awesome regular

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,6 +59,10 @@ export default {
       {
         set: "@fortawesome/free-solid-svg-icons",
         icons: ["fas"]
+      },
+      {
+        set: "@fortawesome/free-regular-svg-icons",
+        icons: ["far"]
       }
     ]
   },
