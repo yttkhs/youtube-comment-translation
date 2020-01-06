726facf291d2f2d05a59c9d3df9cced15828d186
Yuta Takahashi <penityai@gmail.com>
2020-01-07 01:54:35 +0900

    ヘッダーのフォームを調整

--- components/globals/TheHeader.vue
+++ components/globals/TheHeader.vue
@@ -3,8 +3,8 @@
     <h1 class="TheHeader__logo">YOUTUBE<br />COMMENT<br />TRANSLATION</h1>
     <form @submit.prevent="sendURL" class="TheHeader__form">
       <label>
-        <input v-model="url" type="text" />
-        <button type="submit">FETCH</button>
+        <input v-model="url" type="text" placeholder="Search..." />
+        <button type="submit"><fa :icon="['fas', 'search']" /></button>
       </label>
     </form>
   </header>
@@ -38,12 +38,49 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
+  box-shadow: var(--box-shadow);
 
   &__logo {
     font-size: 10px;
     letter-spacing: 0.2em;
     line-height: 140%;
     color: $COLOR_YOUTUBE;
+  }
+
+  &__form {
+    margin-left: auto;
+    margin-right: auto;
+
+    label {
+      display: flex;
+    }
+
+    input[type="text"] {
+      width: 250px;
+      border: 1px solid var(--color-gray);
+      background-color: var(--color-base);
+      color: var(--color-text);
+      font-size: 16px;
+      line-height: 20px;
+      appearance: none;
+      padding: 5px 10px;
+      -webkit-font-smoothing: antialiased;
+      border-radius: 3px 0 0 3px;
+      outline: none;
+
+      &:focus {
+        border: 1px solid var(--color-blue);
+      }
+    }
+
+    button {
+      text-align: center;
+      width: 50px;
+      color: var(--color-base);
+      background-color: var(--color-gray);
+      border-radius: 0 3px 3px 0;
+      font-size: 14px;
+    }
   }
 }
 </style>
