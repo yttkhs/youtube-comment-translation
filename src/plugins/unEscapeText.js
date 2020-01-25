851d140fb9d9dd0f54de91481eb2af3c4d8f6791
Yuta Takahashi <penityai@gmail.com>
2020-01-26 03:08:38 +0900

    Change unEscapeText to inject

--- src/plugins/unEscapeText.js
+++ src/plugins/unEscapeText.js
@@ -1,8 +1,11 @@
-export default function(str) {
-  return str
-    .replace(/(&lt;)/g, "<")
-    .replace(/(&gt;)/g, ">")
-    .replace(/(&quot;)/g, '"')
-    .replace(/(&#39;)/g, "'")
-    .replace(/(&amp;)/g, "&");
-}
+export default (str, inject) => {
+  const unEscapeText = str =>
+    str
+      .replace(/(&lt;)/g, "<")
+      .replace(/(&gt;)/g, ">")
+      .replace(/(&quot;)/g, '"')
+      .replace(/(&#39;)/g, "'")
+      .replace(/(&amp;)/g, "&");
+
+  inject("unEscapeText", unEscapeText);
+};
